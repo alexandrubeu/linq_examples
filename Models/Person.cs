6edcddea920827f7bdd1ca355f8375c128d4709b
dropletalexbeu <alexandru.beu@droplet.page>
2026-07-08 09:44:01 +0300

    udate age.

--- Models/Person.cs
+++ Models/Person.cs
@@ -3,7 +3,7 @@ namespace LinqExamples.Models;
 internal class Person
 {
     public string Name { get; set; }
-    public string Age { get; set; }
+    public int Age { get; set; }
 
     public override string ToString()
     {
